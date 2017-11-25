9b75608ece2ba4339b66967b93829eeb77e72a2c
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2017-11-25 16:46:01 +0100

    Update pat_text.php
    Nope. According to the lAtts() function, there is no array size limit (see: http://phpcrossref.com/xref/textpattern/textpattern/lib/txplib_misc.php.html#l2134).
    Default size is set by the memory_limit parameter into the php.ini file (8M before PHP 5.2; then 128M).
    
    Add a new attribute 'exclusive'.

--- pat_text.php
+++ pat_text.php
@@ -7,7 +7,7 @@
  * @type:    Public
  * @prefs:   no
  * @order:   5
- * @version: 0.1.4
+ * @version: 0.1.6
  * @license: GPLv2
 */
 
@@ -24,9 +24,10 @@ if (class_exists('\Textpattern\Tag\Registry')) {
 /**
  * Main plugin function to display translations by choosen country code
  *
- * @param  $items string Comma separated list of translations
- * @param  $lang  string Country code (ISO2)
- * @return string The corresponding string from the list
+ * @param  $items string      Comma separated list of translations
+ * @param  $lang  string      Country code (ISO2)
+ * @pram   $exclusive boolean Overwrites the current language and sniffs ?lang= to get a new one
+ * @return string             The corresponding string from the list
  *
  */
 function pat_text($atts)
@@ -35,25 +36,32 @@ function pat_text($atts)
 	$current = substr(get_pref('language'), 0, 2);
 
 	extract(lAtts(array(
-		'items' => $current.' Nothing to display 😢',
-		'lang'  => $current,
+		'items'      => $current.' Nothing to display 😢',
+		'lang'       => $current,
+		'exclusive'  => false,
 	), $atts));
 
 	// Display errors
 	strlen($lang) > 2 ? trigger_error( gTxt('invalid_attribute_value', array('{name}' => 'lang')), E_USER_WARNING ) : '';
 	assert_string($items);
 
+	// Overwritting
+	if (false != $exclusive)
+		$lang = gps('lang');
+
 	// Loop into the items list converted as an array
-	$list = explode( ',', preg_replace('/\s*,\s*/', ',', $items) );
-	if (strlen($atts['items']) < 264) {
+	if ($items) {
+		$list = explode( ',', preg_replace('/\s*,\s*/', ',', $items) );
 		foreach ($list as $value) {
 			if (substr($value, 0, 2) == $lang) {
 				$out = substr($value, 3);
 			}
 		}
+
 		// Return the matching string or a fallback
 		return $out ? $out : substr($list[0], 3);
 	}
 	else
 		return '';
+
 }
