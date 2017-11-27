9c794ed2353d6d73b19516c3381b05ed374e7dd6
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2017-11-27 17:58:26 +0100

    Update pat_text.php
    Typo.

--- pat_text.php
+++ pat_text.php
@@ -26,7 +26,7 @@ if (class_exists('\Textpattern\Tag\Registry')) {
  *
  * @param  $items string      Comma separated list of translations
  * @param  $lang  string      Country code (ISO2)
- * @pram   $exclusive boolean Overwrites the current language and sniffs ?lang= to get a new one
+ * @param  $exclusive boolean Overwrites the current language and sniffs ?lang= to get a new one
  * @return string             The corresponding string from the list
  *
  */
@@ -51,12 +51,10 @@ function pat_text($atts)
 		$list = explode( ',', preg_replace('/\s*,\s*/', ',', $items) );
 		
 		foreach ($list as $value) {
-			if (false != $exclusive && $current == $lang) {
+			if (false != $exclusive && $current == $lang)
 				$out = ' ';
-			}
-			elseif (substr($value, 0, 2) == $lang) {
+			elseif (substr($value, 0, 2) == $lang)
 				$out = substr($value, 3);
-			}
 		}
 		// Return the matching string or a fallback
 		return $out ? $out : substr($list[0], 3);
